57d1957164d03eff1d6bfc101bb2350549cb25f6
Shai <kingpepg4@gmail.com>
2020-03-03 08:57:46 +0200

    add coins class

---
+++ JS/coinClasses.js
@@ -0,0 +1,36 @@
+export class CryptoCoinObj {
+  constructor(img, currPrice, currTime) {
+    this.image = img;
+    this.price = {
+      Usd: `${currPrice.usd}`,
+      Eur: `${currPrice.eur}`,
+      Ils: `${currPrice.ils}`
+    };
+    this.time = currTime;
+  }
+  showCoinInfo() {
+    console.log(`
+    Current Price in US Dollar: ${this.usd}\n
+    Current Price in Euru: ${this.eur}\n
+    Current Price in IL Shekel: ${this.ils}\n
+    `);
+  }
+}
+
+export class Coins {
+  static arrAllListOfCoins = [];
+  static arrCoinsWithExtraParam = [];
+
+  static addToList(obj) {
+    this.arrAllListOfCoins.push(obj);
+  }
+  static getList() {
+    return this.arrAllListOfCoins;
+  }
+  static addExtraParam(obj) {
+    this.arrCoinsWithExtraParam.push(obj);
+  }
+  static getArrWithExtraParam() {
+    return this.arrCoinsWithExtraParam;
+  }
+}
